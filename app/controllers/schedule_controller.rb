85b9b84d0009fa8d59946cde391f4622e9b45f6b
Jeff Gerlach <jeffgerlach06@gmail.com>
2016-04-17 20:46:36 -0600

    Add in schedule model for each event day, add show view for each day

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,4 +1,17 @@
 class ScheduleController < ApplicationController
   def index
+    #@events = Event.all.order("start_time ASC")
+    #@date_list = []
+    #@events.each do |event|
+      #event_day = event.start_time.strftime("%d %b %Y")
+      #@date_list | [event_day] #add date to list if unique <- wasn't working, need to investigate
+      # there is a more efficient way to do this, would update given time
+      #@date_list.push(event_day) unless @date_list.include?(event_day)
+    #end
+    @schedules = Schedule.all.order("event_date ASC")
+  end
+
+  def show
+    @current_events = Event.where("start_time >= ? AND start_time <= ?", "08 Dec 2015".to_datetime, "08 Dec 2015".to_datetime + 1.days) #params[:event_date])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418012140) do
+ActiveRecord::Schema.define(version: 20160418032249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,5 +28,11 @@ ActiveRecord::Schema.define(version: 20160418012140) do
 
   add_index "events", ["course_name"], name: "index_events_on_course_name", using: :btree
   add_index "events", ["track_name"], name: "index_events_on_track_name", using: :btree
+
+  create_table "schedules", force: :cascade do |t|
+    t.date     "event_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
